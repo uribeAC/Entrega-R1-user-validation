3455102e1ae7413ac8f960aace99611a45f467f7
Alex Uribe <repzyyog@gmail.com>
2025-02-09 13:14:33 +0100

    Commit nuevo RegEx

--- js/index.js
+++ js/index.js
@@ -1,33 +1,29 @@
-const userName = "donPepito";
-const userPassword = "asdÍd5eñito";
-const repeatUserPassword = "asdÍd5eñito";
-const userAge = "22";
+const username = "donPepito";
+const userPassword = "adc53áÑÙ";
+const repeatedUserPassword = "adc53áÑÙ";
+const userAge = 22;
 
-//password check
-const regexpLength = /[a-ñA-Ñ0-9À-ú]{8,}/g;
-const regexpUpper = /[A-ÑÀ-Ú]/g;
-const regexpNumber = /[0-9]/g;
+const lengthRegEx = /[a-zA-Z0-9À-ú]{8,}/g;
+const upperCaseRegEx = /[A-ZÀ-Ú]/g;
+const numberRegEx = /[0-9]/g;
 
-const passwordLength = regexpLength.test(userPassword);
-const passwordUpper = regexpUpper.test(userPassword);
-const passwordNumber = regexpNumber.test(userPassword);
+const hasPasswordLength = lengthRegEx.test(userPassword);
+const hasPasswordUpperCase = upperCaseRegEx.test(userPassword);
+const hasPasswordNumber = numberRegEx.test(userPassword);
 
-//print
-console.log(`El nombre de usuario es válido: ${userName.length >= 5}`);
+const minimumUsernameLength = 5;
+console.log(
+  `El nombre de usuario es válido: ${username.length >= minimumUsernameLength}`
+);
 console.log(
   `La contraseña es válida: ${
-    passwordLength && passwordNumber && passwordUpper
+    hasPasswordLength && hasPasswordUpperCase && hasPasswordNumber
   }`
 );
 console.log(
   `La repetición de la contraseña es válida: ${
-    repeatUserPassword === userPassword
+    repeatedUserPassword === userPassword
   }`
 );
-console.log(`Es mayor de edad: ${Number(userAge) >= 18}`);
-
-//chequeos
-/*console.log(passwordLength);
-console.log(passwordUpper);
-console.log(passwordNumber);
-console.log(passwordLength && passwordNumber && passwordUpper); */
+const minimumAdultAge = 18;
+console.log(`Es mayor de edad: ${userAge >= minimumAdultAge}`);
